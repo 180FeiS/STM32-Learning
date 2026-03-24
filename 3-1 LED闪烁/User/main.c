232a2f823c8fa58ecbe2413cdd7ed514e33ba927
@ASDFAASDFWE <2459555405@qq.com>
2026-03-24 13:28:24 +0800

    更新 LED 控制逻辑，修改 GPIO 初始化为全引脚，添加流水灯效果，循环点亮 LED。

--- 3-1 LED闪烁/User/main.c
+++ 3-1 LED闪烁/User/main.c
@@ -6,18 +6,24 @@ int main(void)
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA,ENABLE);
 	GPIO_InitTypeDef  GPIO_InitStructure;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_All;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz; 
 	GPIO_Init(GPIOA,&GPIO_InitStructure);
 	//GPIO_SetBits(GPIOA,GPIO_Pin_0);
 	//GPIO_ResetBits(GPIOA,GPIO_Pin_0);
-	GPIO_WriteBit(GPIOA,GPIO_Pin_0,Bit_RESET);
+	//GPIO_WriteBit(GPIOA,GPIO_Pin_0,Bit_RESET);
 
-	while(1)
+	/* 流水灯：每次只点亮一位，循环左移；若 LED 为低电平点亮，把下一行改成 GPIO_Write(GPIOA, ~Bit_pin & 0x00FF) */
+	uint16_t Bit_pin = 0x0001;
+
+	while (1)
 	{
-	GPIO_WriteBit(GPIOA,GPIO_Pin_0,Bit_RESET);
-	Delay_ms(500);
-	GPIO_WriteBit(GPIOA,GPIO_Pin_0,Bit_SET);
-	Delay_ms(500);
+		GPIO_Write(GPIOA, ~Bit_pin);
+		Delay_ms(500);
+		Bit_pin <<= 1;
+		if (Bit_pin > 0x0080)
+		{
+			Bit_pin = 0x0001;
+		}
 	}
 }
