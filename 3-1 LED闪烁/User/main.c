5e77db26de0f91b30b48fce547e59c8575b601d1
@ASDFAASDFWE <2459555405@qq.com>
2026-03-25 08:54:13 +0800

    按键

--- 3-1 LED闪烁/User/main.c
+++ 3-1 LED闪烁/User/main.c
@@ -1,24 +1,25 @@
 #include "stm32f10x.h"                  // Device header
 #include "Delay.h"
-
+#include "LED.h"
+#include "Key.h"
 int main(void)
 {
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB,ENABLE);
-	GPIO_InitTypeDef  GPIO_InitStructure;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_All;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz; 
-	GPIO_Init(GPIOB,&GPIO_InitStructure);
-	//GPIO_SetBits(GPIOA,GPIO_Pin_0);
-	//GPIO_ResetBits(GPIOA,GPIO_Pin_0);
-	//GPIO_WriteBit(GPIOA,GPIO_Pin_0,Bit_RESET);
-
+	LED_Init();	
+	Key_Init();
+	
+	uint8_t key_num = 0;
+	
 	while (1)
 	{
-		GPIO_WriteBit(GPIOB,GPIO_Pin_12,Bit_RESET);
-		Delay_ms(500);
-		GPIO_WriteBit(GPIOB,GPIO_Pin_12,Bit_SET);
-		Delay_ms(500);
+		key_num = KEY_GetNum();
+		if(key_num == 1)
+		{
+			LED1_Turn();
+		}
+		else if(key_num == 2)
+		{
+			LED2_Turn();
+		}
 	}
 
 }
